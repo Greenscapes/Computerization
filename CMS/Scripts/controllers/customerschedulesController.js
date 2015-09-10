6a9779fb73ab4d10c71b08166e76d1c07bd77042
satheesh.ns@gmail.com <pallipadi03>
2015-09-10 16:24:38 -0500

    Added Schedule links to complete the flow

---
+++ CMS/Scripts/controllers/customerschedulesController.js
@@ -0,0 +1,15 @@
+﻿function CustomerSchedulesController( $scope, $resource, $location ) {
+    var customerschedulesResource = $resource( '/api/customerschedules' );
+   
+    $scope.properties = customerschedulesResource.query( function () { } );
+  }
+
+CustomerSchedulesController.$inject = ['$scope', '$resource', '$location'];
+
+app.controller( 'CustomerSchedulesController', CustomerSchedulesController );
+
+
+
+
+
+
---
+++ CMS/Scripts/controllers/eventschedulesController.js
@@ -0,0 +1,15 @@
+﻿function EventSchedulesController( $scope, $resource, $location ) {
+    var eventschedulesResource = $resource( '/api/eventschedules' );
+   
+        $scope.eventSchedules = eventschedulesResource.query( function () { } );
+  }
+
+EventSchedulesController.$inject = ['$scope', '$resource', '$location'];
+
+app.controller( 'EventSchedulesController', EventSchedulesController );
+
+
+
+
+
+
