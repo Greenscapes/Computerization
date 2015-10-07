36082f0782e6b94dedd76071c4af416e98f7ed74
satheesh.ns@gmail.com <pallipadi03>
2015-10-07 15:54:59 -0500

    Added Crew dashboard screen and basic functionality

--- CMS/Controllers/EventSchedulesController.cs
+++ CMS/Controllers/EventSchedulesController.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using CMS.Models;
+using System.Globalization;
 
 namespace CMS.Controllers
 {
@@ -76,6 +77,66 @@ namespace CMS.Controllers
             }
 
         }
+        // GET: api/eventschedules
+        [Route("{year:int}/{month:int}/{date:int}/{crewid:int}/events")]
+        public IEnumerable<EventDetails> GetEventSchedules(int year, int month, int date, int crewid)
+        {
+            try
+            {
+                //int crewid = 3;
+                DateTime selectedDate = new DateTime(year, month, date);
+                DateTime from = GetFirstDayOfWeek(selectedDate, null);
+                DateTime to = GetLastDayOfWeek(selectedDate,null);
+                List<EventDetails> eventDetails = new List<EventDetails>();
+
+                var eventSchedules = db.EventSchedules.Where(ev => (ev.StartTime <= from) && (ev.EndTime >= from)||
+                                                                   (ev.StartTime <= to) && (ev.EndTime >= to)||
+                                                                    (ev.StartTime >= from) && (ev.EndTime <= to)
+                    ).ToList();
+           
+               foreach (var eventVal in eventSchedules)
+               {
+                   PropertyTask propTask = db.PropertyTasks.Single(pt => pt.Id == eventVal.PropertyTaskId);
+                   if(propTask.Crews== null || propTask.Crews.Count < 1)
+                   {
+                       continue;
+                   }
+                   Crew crewVal = propTask.Crews.First(c=>c.Id == crewid );
+                   if(crewVal == null)
+                   {
+                       continue;
+                   }
+                   EventDetails eventDetail = new EventDetails();
+                   eventDetail.EventId = eventVal.Id;
+                   eventDetail.StartTime = eventVal.StartTime;
+                   eventDetail.EndTime = eventVal.EndTime;
+                   eventDetail.Description = eventVal.Title;
+                   eventVal.PropertyTask =propTask;
+                   eventDetail.TaskId = propTask.Id;
+                   eventDetail.PropertyId = propTask.PropertyTaskList.Property.Id;
+                   eventDetail.PropertyAddress = propTask.PropertyTaskList.Property.Address1 + " "+
+                                                 propTask.PropertyTaskList.Property.Address2 + " "+
+                                                     propTask.PropertyTaskList.Property.City + " "+
+                                                         propTask.PropertyTaskList.Property.State + " "+
+                                                             propTask.PropertyTaskList.Property.Zip ;
+
+                   eventDetail.PropertyName = eventVal.PropertyTask.PropertyTaskList.Property.Name;
+                   eventDetail.PropertyRefNumber = eventVal.PropertyTask.PropertyTaskList.Property.PropertyRefNumber;
+                   eventDetail.Crew=crewVal;
+                   eventDetails.Add(eventDetail);
+               }  
+                  
+
+                return eventDetails;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+        /*
         // [Route("{id:int}/eventschedules'")]int taskId
         [Route("~/api/properties/:propertyId/tasklists/{taskListId:int}/tasks/{taskId:int}/eventschedules")]
         public IQueryable<EventScheduleDetails> GetEventSchedules(int propertyId, int taskListId,int taskId)
@@ -91,7 +152,7 @@ namespace CMS.Controllers
 
 
             return result;
-        }
+        }*/
         //[Route("")]
         //public IQueryable<EventSchedule> GetEventSchedules()
         //{
@@ -189,6 +250,36 @@ namespace CMS.Controllers
         {
             return db.EventSchedules.Count(e => e.Id == id) > 0;
         }
+
+        public static DateTime GetFirstDayOfWeek(DateTime dayInWeek, CultureInfo cultureInfo)
+        {
+            if (cultureInfo == null)
+            {
+                cultureInfo = CultureInfo.CurrentCulture;
+            }
+
+            DayOfWeek firstDay = cultureInfo.DateTimeFormat.FirstDayOfWeek;
+            DateTime firstDayInWeek = dayInWeek.Date;
+            while (firstDayInWeek.DayOfWeek != firstDay)
+                firstDayInWeek = firstDayInWeek.AddDays(-1);
+
+            return firstDayInWeek;
+        }
+
+        public static DateTime GetLastDayOfWeek(DateTime dayInWeek, CultureInfo cultureInfo)
+        {
+            if (cultureInfo == null)
+            {
+                cultureInfo = CultureInfo.CurrentCulture;
+            }
+
+            DayOfWeek firstDay = cultureInfo.DateTimeFormat.FirstDayOfWeek;
+            DateTime firstDayInWeek = dayInWeek.Date;
+            while (firstDayInWeek.DayOfWeek != firstDay)
+                firstDayInWeek = firstDayInWeek.AddDays(-1);
+
+            return firstDayInWeek.AddDays(7);
+        }
     }
 
     public class EventScheduleDetails
@@ -198,4 +289,23 @@ namespace CMS.Controllers
         public string start { get; set; }
         public string end { get; set; }
     }
+
+    public class EventDetails
+    {
+        public int EventId { get; set; }
+        public int TaskId { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public int PropertyId { get; set; }
+        public string PropertyName { get; set; }
+        public string PropertyRefNumber { get; set; }
+        public string PropertyAddress { get; set; }
+
+        public Crew Crew { get; set; }
+       
+    }
+     
 }
